a67052e2e4f12256348505436de0816aeab79bad
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-18 13:15:29 -0600

    created query to get launch information from api and rendered data to frontend

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql")
-const app = express()
-const schema = require('./schema')
+const graphqlHTTP = require("express-graphql");
+const schema = require('./schema');
+const cors = require('cors');
+
+const app = express();
+
+//Allow cross-origin
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
