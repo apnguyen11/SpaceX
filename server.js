e3fe648968dd86a38eeb35b46942593bc5218f91
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-19 15:35:54 -0600

    configure environment to serve build folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema');
 const cors = require('cors');
+const path = require('path')
 
 const app = express();
 
@@ -14,5 +15,14 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static( 'client/build'))
+
+    app.get('*', () => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) // relative path
+    })
+}
+
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
